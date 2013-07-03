ae59f79af396eaba824480be2c404ca377c940fd
Sevion <Hero-Of-Winds@hotmail.com>
2013-07-03 12:02:48 -0700

    RTS Camera Movement Update
    Fixed a problem with the zooming and fixed clamping issues with higher
    scroll speeds.

--- RTSCameraMovement.cs
+++ RTSCameraMovement.cs
@@ -2,7 +2,7 @@
  * Name: RTS Camera Movement
  * Author: James 'Sevion' Nhan
  * Date: 02/07/2013
- * Version: 1.0.0.0
+ * Version: 1.0.0.1
  * Description:
  * 		This is a simple RTS movement script that handles
  * 		arrow keys, mouse edge, and scroll wheel zooming.
@@ -14,40 +14,41 @@ using System.Collections;
 public class RTSCameraMovement : MonoBehaviour {
 	public const int SCROLLDISTANCE = 5;
 	public const float SCROLLSPEED = 1.0f;
-	public const float ZOOMSPEED = 5.0f;
+	public const float ZOOMSPEED = 50.0f;
+    public const float ZOOMROTSPEED = 5.0f;
 	public const float MAXSCROLL = 50.0f;
 	private float DEFAULTFOV = 0.0f;
-	
+
 	void Start() {
 		DEFAULTFOV = Camera.main.fieldOfView;
 	}
-	
+
 	// Update is called once per frame
 	void Update() {
 		// The camera's X rotation in radians
 		float CameraAngleFromPerpendicular = Camera.main.transform.eulerAngles.x * Mathf.PI / 180.0f;
-		
+
 		// Get the input data in the respective axes
 		float xAxisValue = Input.GetAxis("Horizontal");
 		// The Y and Z axes need to be adjusted if there's any X rotation in the camera
 		// Y corresponds to Sin and Z corresponds to Cos
 	    float yAxisValue = Input.GetAxis("Vertical") * Mathf.Sin(CameraAngleFromPerpendicular);
 		float zAxisValue = Input.GetAxis("Vertical") * Mathf.Cos(CameraAngleFromPerpendicular);
-		
+
 		// Check that there is a current camera and then transform it to the new vector
 	    if(Camera.main != null){
 	        Camera.main.transform.Translate(new Vector3(xAxisValue, yAxisValue, zAxisValue));
 	    }
-		
+
 		// Get the mouse's coordinates
 		float mousePosX = Input.mousePosition.x;
         float mousePosY = Input.mousePosition.y;
-		
+
 		// Reset these values
 		xAxisValue = 0.0f;
 		yAxisValue = 0.0f;
 		zAxisValue = 0.0f;
- 
+
 		// X axis transformation
         if ((mousePosX < SCROLLDISTANCE)) {
 			xAxisValue = -SCROLLSPEED;
@@ -64,25 +65,35 @@ public class RTSCameraMovement : MonoBehaviour {
 			yAxisValue = SCROLLSPEED * Mathf.Sin(CameraAngleFromPerpendicular);
 			zAxisValue = SCROLLSPEED * Mathf.Cos(CameraAngleFromPerpendicular);
         }
-		
+
 		// Check that there is a current camera and then transform it to the new vector
 	    if(Camera.main != null){
 	        Camera.main.transform.Translate(new Vector3(xAxisValue, yAxisValue, zAxisValue));
 		}
-		
+
 		// Scrolling Zoom
 		// Scrolling up or down
-		if (Mathf.Sign(Input.GetAxis("Mouse ScrollWheel")) == 1) {
+		if (Input.GetAxis("Mouse ScrollWheel") > 0) {
 			// If it's in the zoom range, then allow to zoom else set to min zoom
 			if (Camera.main.fieldOfView > DEFAULTFOV - MAXSCROLL) {
-				Camera.main.fieldOfView -= Input.GetAxis("Mouse ScrollWheel") * ZOOMSPEED;
+                // Clamping
+                if (Camera.main.fieldOfView - Input.GetAxis("Mouse ScrollWheel") * ZOOMSPEED >= DEFAULTFOV - MAXSCROLL) {
+				    Camera.main.fieldOfView -= Input.GetAxis("Mouse ScrollWheel") * ZOOMSPEED;
+                } else {
+                    Camera.main.fieldOfView = DEFAULTFOV - MAXSCROLL;
+                }
 			} else {
 				Camera.main.fieldOfView = DEFAULTFOV - MAXSCROLL;
 			}
 		} else {
 			// If it's in the zoom range, then allow to zoom else set to max zoom
 			if (Camera.main.fieldOfView < DEFAULTFOV) {
-				Camera.main.fieldOfView -= Input.GetAxis("Mouse ScrollWheel") * ZOOMSPEED;
+                // Clamping
+                if (Camera.main.fieldOfView - Input.GetAxis("Mouse ScrollWheel") * ZOOMSPEED <= DEFAULTFOV) {
+				    Camera.main.fieldOfView -= Input.GetAxis("Mouse ScrollWheel") * ZOOMSPEED;
+                } else {
+                    Camera.main.fieldOfView = DEFAULTFOV;
+                }
 			} else {
 				Camera.main.fieldOfView = DEFAULTFOV;
 			}
